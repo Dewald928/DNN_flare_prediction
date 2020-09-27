48494398f065c944068e45a9b9fabd858d18663a
Dewald928 <dewald123@rocketmail.com>
2020-09-27 09:24:23 +0200

    test working?
    Took 1 minute

--- tests/test_classifier.py
+++ tests/test_classifier.py
@@ -1,13 +1,14 @@
 from pytorch_lightning import Trainer, seed_everything
 
 from project.lit_classifier_main import LitClassifier
+from project.datamodule import MNISTDataModule
 
 
 def test_lit_classifier():
     seed_everything(1234)
 
     model = LitClassifier()
-    train, val, test = mnist()
+    dm = MNISTDataModule()
     trainer = Trainer(limit_train_batches=50, limit_val_batches=20, max_epochs=2)
     trainer.fit(model, dm)
 
